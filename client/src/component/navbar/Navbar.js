ad0aa4f745f89f648ec12f90aaa0d970a2247113
Love to code my life <47113916+vaishnavan@users.noreply.github.com>
2021-08-09 09:28:07 +0530

    nav-design completed

---
+++ client/src/component/navbar/Navbar.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './navbar.css';
+
+function Navbar() {
+    return (
+        <div className="nav-design">
+            {/* olx navbar creation */}
+            <div className="container nav-main">
+                <div className="nav-logo">
+                    <h3>O<sub>L</sub>X</h3>
+                </div>
+
+                <div className="nav-select">
+                    <div>
+                        <span><i className="fa fa-search"></i></span>
+                    </div>
+                    <div>
+                        <span><input type="text" placeholder="Search City, area or locality" /></span>
+                    </div>
+                    <div>
+                        <span className="nav-select-angle"><i className="fa fa-angle-down"></i></span>
+                    </div>
+                </div>
+
+                <div className="nav-search">
+                    <div className="nav-search-input">
+                        <input type="text" placeholder="Find Cars, Mobile Phones and more..." />
+                    </div>
+                    <div className="nav-search-btn">
+                        <button><i className="fa fa-search"></i></button>
+                    </div>
+                </div>
+                <div className="nav-langage">
+                    <div>
+                        <span className="nav-lang-select">English</span>
+                    </div>
+                    <div>
+                        <span className="nav-lang-icon"><i className="fa fa-angle-down"></i></span>
+                    </div>
+                </div>
+                <div className="nav-login">
+                    <a href="#">Login</a>
+                </div>
+
+                <div className="nav-sell">
+                    <div className="nav-sell-add-icon">
+                        <span>+</span>
+                    </div>
+                    <div className="nav-sell-link">
+                        <a href="#">SELL</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
